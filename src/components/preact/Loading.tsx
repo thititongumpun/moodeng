7f87fbf0ea646676ba74f6528e19b5cc77e39420
thititongumpun <thiti180536@gmail.com>
2024-10-02 18:17:57 +0700

    lazy import

---
+++ src/components/preact/Loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <span className="loading loading-infinity w-32"></span>
+    </div>
+  );
+}
--- src/components/preact/PreactApp.tsx
+++ src/components/preact/PreactApp.tsx
@@ -1,12 +1,13 @@
 import { useState } from "preact/hooks";
-import Search from "@/components/preact/Search";
-import Meme from "@/components/preact/Meme";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Suspense, lazy } from "preact/compat";
 
-type Props = {};
+const Search = lazy(() => import("@/components/preact/Search"));
+const Meme = lazy(() => import("@/components/preact/Meme"));
+const Loading = lazy(() => import("@/components/preact/Loading"));
 
-export default function PreactApp({}: Props) {
+export default function PreactApp() {
   const client = new QueryClient({
     defaultOptions: {
       queries: {
@@ -27,8 +28,10 @@ const App = () => {
 
   return (
     <div className="p-0">
+      <Suspense fallback={<Loading />}>
       <Search search={search} setSearch={setSearch} />
       <Meme search={search} />
+      </Suspense>
     </div>
   );
 };
