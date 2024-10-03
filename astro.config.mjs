fa2e08beb98642672932ab27df558737bb63718f
Thiti Tongumpun <thiti_t@mfec.co.th>
2024-10-03 14:12:30 +0700

    up moodeng pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,62 @@ import preact from "@astrojs/preact";
 
 import vercel from "@astrojs/vercel/serverless";
 
+import AstroPWA from "@vite-pwa/astro";
+
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  integrations: [tailwind(), preact()],
+  integrations: [
+    tailwind(),
+    preact(),
+    AstroPWA({
+      mode: "production",
+      base: "/",
+      scope: "/",
+      includeAssets: ["favicon.svg"],
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Wcydtt",
+        short_name: "Wcydtt PWA",
+        theme_color: "#8936FF",
+        background_color: "#2EC6FE",
+        description: "This is some miscellaneous app.",
+        orientation: "any",
+        display: "standalone",
+        lang: "en-US",
+        start_url: "/",
+        icons: [
+          {
+            src: "pwa-512x512_rounded.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+      workbox: {
+        navigateFallback: "/",
+        globPatterns: ["**/*.{css,js,html,svg,png,ico,txt}"],
+      },
+      devOptions: {
+        enabled: false,
+        navigateFallbackAllowlist: [/^\//],
+      },
+      experimental: {
+        directoryAndTrailingSlashHandler: true,
+      },
+    }),
+  ],
   server: { port: 5173 },
 
   vite: {
