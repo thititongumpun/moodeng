f6b6f8b2b79811582e76c4692b1872e909c7d993
thititongumpun <thiti180536@gmail.com>
2024-10-06 02:20:24 +0700

    up moodeng optimize package chunk

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
-// @ts-check
+// @ts-check 
 import { defineConfig } from "astro/config";
 
+// Integrations
 import tailwind from "@astrojs/tailwind";
-
 import preact from "@astrojs/preact";
-
 import vercel from "@astrojs/vercel/serverless";
-
 import AstroPWA from "@vite-pwa/astro";
 
 // https://astro.build/config
 export default defineConfig({
+  // Output hybrid mode for SSR and static generation
   output: "hybrid",
+
+  // Integrations for Tailwind, Preact, Vercel, and PWA
   integrations: [
     tailwind(),
     preact(),
@@ -63,8 +64,13 @@ export default defineConfig({
       },
     }),
   ],
-  server: { port: 5173 },
 
+  // Server configuration
+  server: {
+    port: 5173, // Local development port
+  },
+
+  // Vite Configuration
   vite: {
     resolve: {
       alias: {
@@ -73,7 +79,29 @@ export default defineConfig({
         "react/jsx-runtime": "preact/jsx-runtime",
       },
     },
+
+    build: {
+      // Rollup options to manually chunk node_modules
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            // Only split large node_modules into separate chunks
+            if (id.includes("node_modules")) {
+              const chunkName = id
+                .toString()
+                .split("node_modules/")[1]
+                .split("/")[0]
+                .toString();
+              return chunkName;
+            }
+          },
+        },
+      },
+      // Set chunk size warning limit to 500kB
+      chunkSizeWarningLimit: 500,
+    },
   },
 
+  // Vercel Serverless Adapter
   adapter: vercel(),
 });
